dbcc0bd94ab609422875e55c0864fddc3f383ff6
Erdem Meral <erdem_meral@hotmail.com>
2025-01-23 15:04:33 +0300

    server.js

--- backend/server.js
+++ backend/server.js
@@ -368,6 +368,8 @@ app.get('*', (req, res) => {
 });
 
 // Start Server
+startServer();
+
 const port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
